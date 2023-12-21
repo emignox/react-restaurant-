ba830575b5478b2831d4b9885fb5ce0d7ddd460c
emanuele <emanuele971@icloud.com>
2023-12-21 16:30:04 +0100

    pushing layout

--- resto.vite/src/App.tsx
+++ resto.vite/src/App.tsx
@@ -1,18 +1,13 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './home';
 
 function App() {
   return (
-    <Router>
       <div>
         
-          <Route path="/">
             <Home />
-          </Route>
         
       </div>
-    </Router>
   );
 }
 
