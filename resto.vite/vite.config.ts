a2e44aa75fdc888160c589ea9d5f50110d74d5f7
emanuele <emanuele971@icloud.com>
2023-12-22 14:51:42 +0100

    pushing layout

--- resto.vite/vite.config.ts
+++ resto.vite/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ract-restaurnat-',
+  base: '/resto.vite/',
   plugins: [react()],
 })
